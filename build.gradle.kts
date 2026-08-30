0484b3eb8509cef5cbd13ea8bf8d1574d20517c3
liberta09 <kaanokan258@gmail.com>
2026-08-31 00:58:10 +0300

    Fix build: update to current Cloudstream library (recloudstream.cloudstream:library) and Kotlin 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 import com.android.build.gradle.BaseExtension
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 buildscript {
     repositories {
@@ -7,10 +9,11 @@ buildscript {
         mavenCentral()
         maven("https://jitpack.io")
     }
+
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
 
@@ -31,39 +34,48 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        setRepo("https://github.com/liberta09/Hindu-")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/liberta09/Hindu-")
         authors = listOf("liberta09")
     }
 
     android {
         namespace = "com.liberta09"
+
         defaultConfig {
             minSdk = 21
             compileSdkVersion(35)
             targetSdk = 35
         }
+
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_1_8
             targetCompatibility = JavaVersion.VERSION_1_8
         }
-        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile> {
+
+        tasks.withType<KotlinJvmCompile> {
             compilerOptions {
-                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
-                freeCompilerArgs.addAll(listOf("-Xno-call-assertions", "-Xno-param-assertions", "-Xno-receiver-assertions"))
+                jvmTarget.set(JvmTarget.JVM_1_8)
+                freeCompilerArgs.addAll(
+                    "-Xno-call-assertions",
+                    "-Xno-param-assertions",
+                    "-Xno-receiver-assertions"
+                )
             }
         }
     }
 
     dependencies {
-        val cloudstream by configurations
         val implementation by configurations
-        cloudstream("com.lagradost:cloudstream3:pre-release")
+
+        implementation("com.github.recloudstream.cloudstream:library:-SNAPSHOT")
+
         implementation(kotlin("stdlib"))
         implementation("com.github.Blatzar:NiceHttp:0.4.11")
         implementation("org.jsoup:jsoup:1.18.3")
-        implementation("org.jspecify:jspecify:1.0.0")
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
     }
 }
 
-task<Delete>("clean") { delete(rootProject.layout.buildDirectory) }
+task<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
